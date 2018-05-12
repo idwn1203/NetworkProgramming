9ce294ffe694f6cba4b5caf1682f94d3ac550da1
JJunney <qnfgh156@naver.com>
2018-05-12 19:44:09 +0900

    Made Project ChatRoom

--- Project/ChatRoom.c
+++ Project/ChatRoom.c
@@ -2,13 +2,48 @@
 
 extern void WaitingRoom();
 extern char name[5];
+int a;
 
+void Input(){
+	printf("Input : ");
+	scanf("%d",&a);
+	getchar();
+}
+
+void Previous(){
+	WaitingRoom();
+}
+void ChatRoom_Menu(){
+	puts("-------------------------------------------------");
+	puts("1.Input | 2.Previous");
+	puts("-------------------------------------------------");
+}
+void Chat(){
+	printf("name : %s \n",name);
+}
 
 void ChatRoom(){
 	int s;
 	puts("-------------------------------------------------");
 	printf("Total : %d \nNow : %d \n",1,2);
 	puts("-------------------------------------------------");
+	Chat();
 	puts("----------------------------------------------\n");
+	ChatRoom_Menu();
+	while(1){
+		puts("Choose Menu : ");
+		scanf("%d",&s);
+		switch(s){
+			case 1 :
+				Input();
+				system("cls");
+				ChatRoom();
+				break;
+			case 2 :
+				system("cls");
+				Previous();
+				break;
+		}
+	}
 }
 
